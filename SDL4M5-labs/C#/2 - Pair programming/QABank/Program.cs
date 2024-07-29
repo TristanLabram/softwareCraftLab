64266b9637097f459bf6d55d68f4a21311d99aee
Tristan Labram <tristan.labram@live.co.uk>
2024-07-29 14:45:53 +0100

    Added BankAccount class to Program

--- SDL4M5-labs/C#/2 - Pair programming/QABank/Program.cs
+++ SDL4M5-labs/C#/2 - Pair programming/QABank/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,31 @@ namespace QABank
     {
         static void Main(string[] args)
         {
+            BankAccount firstAccount = new BankAccount("Tristan", 5.43);
+            BankAccount secondAccount = new BankAccount("Another Name", 100.58, 5);
+        }
+    }
+
+    class BankAccount
+    {
+        public String AccountName;
+        public double Balance;
+        public int AccountNum;
+        private static int lastAccNum = 100000;
+        public BankAccount(String customerName, double balance){
+            if(!string.IsNullOrWhiteSpace(customerName)){
+                AccountName = customerName;
+                Balance = balance;
+                AccountNum = lastAccNum;
+                lastAccNum ++;
+            }
+        }
+        public BankAccount(String customerName, double balance, int accountNum){
+            if(!string.IsNullOrWhiteSpace(customerName)){
+                AccountName = customerName;
+                Balance = balance;
+                AccountNum = accountNum;
+            }
         }
     }
 }
